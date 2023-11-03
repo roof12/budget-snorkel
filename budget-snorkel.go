dcd74823c0575c207407df424461c34cf5b3d55e
Scott Lewis <scott@roof12.com>
2023-11-03 17:47:09 -0600

    make random moves

--- budget-snorkel.go
+++ budget-snorkel.go
@@ -13,10 +13,16 @@ const version = "0.0.1"
 
 var dbg = false
 
-func handle(line string) {
-	tokens := strings.Split(line, " ")
+func find_move(game *chess.Game) *chess.Move {
+	moves := game.ValidMoves()
+	return moves[0]
+}
 
-	switch tokens[0] {
+func handle(line string, game *chess.Game) *chess.Game {
+	tokens := strings.Split(line, " ")
+	cmd := tokens[0]
+
+	switch cmd {
 	case "uci":
 		fmt.Println("id name Budget Snorkel")
 		fmt.Println("id author Scott Lewis")
@@ -27,11 +33,14 @@ func handle(line string) {
 
 	case "stop":
 
+	case "quit":
+		os.Exit(0)
+
 	case "debug":
 		if len(tokens) == 1 {
 			dbg = !dbg
 		} else {
-			switch tokens[1] {
+			switch cmd {
 			case "on":
 				dbg = true
 			case "off":
@@ -43,22 +52,43 @@ func handle(line string) {
 		fmt.Printf("debug %t\n", dbg)
 
 	case "position":
-		game := chess.NewGame(chess.UseNotation(chess.UCINotation{}))
-		game.MoveStr("e2e4")
-		game.MoveStr("e7e5")
+		if len(tokens) == 1 {
+			return game
+		}
+
+		if tokens[1] == "startpos" {
+			game = chess.NewGame(chess.UseNotation(chess.UCINotation{}))
+		}
+
+		if len(tokens) == 2 {
+			return game
+		}
+
+		for _, move := range tokens[2:] {
+			game.MoveStr(move)
+			// fmt.Println(game.Position().Board().Draw())
+		}
+
+	case "show":
 		fmt.Println(game.Position().Board().Draw())
 
-	case "quit":
-		os.Exit(0)
+	case "go":
+		move := find_move(game)
+		fmt.Printf("info score cp %d\n", 100)
+		fmt.Printf("bestmove %s\n", move.String())
+
 	default:
+		// ignore
 	}
+	return game
 }
 
 func main() {
 	fmt.Printf("Budget Snorkel v%s\n", version)
+	game := chess.NewGame(chess.UseNotation(chess.UCINotation{}))
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		handle(scanner.Text())
+		game = handle(scanner.Text(), game)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
